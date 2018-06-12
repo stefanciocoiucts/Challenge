38f8943f72ab852579a302eed4c4ea21cfd0f07f
stefanciocoiucts <32101@corbytechnicalschool.org>
2018-06-12 16:16:52 +0100

    Initial check-in of module Challenge

---
+++ Challenge/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Challenge
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
